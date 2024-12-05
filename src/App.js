be294a9eda432d4505c68c01ba5a6813d47c2f54
dougpiovesani <douglasrpiovesani@gmail.com>
2024-12-04 22:27:17 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import Header2 from "./components/header2";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+
+function App() {
+  return (
+    <div className="App">
+      <Header2 />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
